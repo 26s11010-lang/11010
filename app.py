6b46fbfad1098e93201907790ad7cb2666ec549d
26s11010-lang <26s11010@guro.sen.hs.kr>
2026-07-20 11:21:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,8 @@ creativity = st.slider("난이도", 0, 100, 50)
 creativity = st.slider("점수", 0, 100, 50)
 user_id = st.text_input("소감", placeholder="example_user")
 
-if st.button("질문 전송하기"):
+if st.button("확인"):
     if agree:
-        st.success(f"성공적으로 전송되었습니다! ({user_id}님)")
-        st.markdown(f"""
-        * **질문 내용:** {question}
-        * **선택 모델:** `{ai_model}` | **말투:** `{tone}`
-        * **활성화 기능:** {', '.join(features) if features else '없음'}
-        * **창의성:** `{creativity}%` | **처리 속도:** `{ai_speed}`
-        """)
-        
-        if age < 14:
-            st.info("참고: 14세 미만 사용자이므로 보호자 모드가 활성화됩니다.")
-    else:
-        st.error("⚠️ 동의 항목에 체크해야 전송이 가능합니다.")
+        st.success(f"{name} / ({grade}학년) / {cls}반 / {level}" )
+        st.markdown(f"점수: '{score}':)
+        st.info(f"소감: {text)")
