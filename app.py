daa6a33f1b458aa416c70a4ab61f36386c52ca4b
26s11010-lang <26s11010@guro.sen.hs.kr>
2026-07-20 11:26:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.markdown("# 앱 UI 만들기")
 import streamlit as st
 
-user_id = st.text_input("이름", placeholder="example_user")
+name = st.text_input("이름", placeholder="example_user")
 grade = st. radio("학년", [1,2,3,], horizontal=True)
-user_id = st.text_input("반", placeholder="example_user")
+cls= st.text_input("반", placeholder="example_user")
 
 st.header("2. 챗봇 설정")
-creativity = st.slider("난이도", 0, 100, 50)
-creativity = st.slider("점수", 0, 100, 50)
-user_id = st.text_input("소감", placeholder="example_user")
+level = st.slider("난이도", 0, 100, 50)
+level = st.slider("점수", 0, 100, 50)
+text = st.text_input("소감", placeholder="example_user")
 
 if st.button("확인"):
     st.success(f"{name} / ({grade}학년) / {cls}반 / {level}" )
