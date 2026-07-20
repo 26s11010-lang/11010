64a68b394d0d2ca9a3506df6b28acda507c6f9ad
26s11010-lang <26s11010@guro.sen.hs.kr>
2026-07-20 11:40:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 
-st.markdown("# 앱 UI 만들기")
-name = st.text_input("이름", placeholder="이름을 쓰세요")
-grade = st. radio("학년", [1,2,3,], horizontal=True)
-cls= st.text_input("반", placeholder="반을 쓰세요")
+with st.sidebar:
+    st.header("프로필")
+    user_name = st.text.input("닉네임")
+    weather = st.selectbax("오늘 날씨", ["맑음", "흐림", "비/눈", "매우 추움"])
+    st.markdown("---")
+    st.info(f"반기워요, {user_name}님! 오늘 날씨는 '{weather}' 이네요! ")
 
-st.header("2. 챗봇 설정")
-level = st.slider("난이도", 0, 100, 50)
-score = st.slider("점수", 0, 100, 50)
-text = st.text_input("소감", placeholder="소감을 입력해주세요")
-
-if st.button("확인"):
-    st.success(f"{name} / ({grade}학년) / {cls}반 / {level}" )
-    st.markdown(f"점수: '{score}'")
-    st.info(f"소감: {text}")
+st.title(" AI 코디메이커")
+st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요!")
+    
+    
