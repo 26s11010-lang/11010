12e3d4affefaa446d0ae966330e58c6a75c4396b
26s11010-lang <26s11010@guro.sen.hs.kr>
2026-07-20 11:24:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ creativity = st.slider("점수", 0, 100, 50)
 user_id = st.text_input("소감", placeholder="example_user")
 
 if st.button("확인"):
-    if agree:
-        st.success(f"{name} / ({grade}학년) / {cls}반 / {level}" )
-        st.markdown(f"점수: '{score}'")
-        st.info(f"소감: {text}")
+    st.success(f"{name} / ({grade}학년) / {cls}반 / {level}" )
+    st.markdown(f"점수: '{score}'")
+    st.info(f"소감: {text}")
