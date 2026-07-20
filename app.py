19f6a12e854da6935126168cb182f0be48a9e4dc
26s11010-lang <26s11010@guro.sen.hs.kr>
2026-07-20 11:16:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.markdown("# 앱 UI 만들기")
 import streamlit as st
 
 user_id = st.text_input("이름", placeholder="example_user")
-creativity = st.slider("학년", 1, 2, 3)
+grade = st. radio("학년", [1,2,3,], horizontal=True)
 user_id = st.text_input("반", placeholder="example_user")
 
 st.header("2. 챗봇 설정")
